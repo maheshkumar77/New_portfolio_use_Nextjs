e12e7a36204f248f5dd396a5c871b2c08938c91e
maheshkumar77 <149643073+maheshkumar77@users.noreply.github.com>
2025-02-14 11:46:24 +0530

    Add files via upload

---
+++ data/data.tsx
@@ -0,0 +1,51 @@
+
+
+export const projectdata = [
+  {
+    id: 1,
+    projectname: "Realtime Chat Application",
+    projectdesc:`A real-time chat application built with React.js, Tailwind CSS, Node.js, Express.js, and MongoDB, enabling users to send and receive messages instantly. It supports user authentication,
+     private rooms, and a responsive design for seamless communication.`,
+    projectlink1: "https://github.com/maheshkumar77/Real--time-Chat-Application.git",
+    projectlink2l: "https://real-time-chata-pplication.netlify.app/login",
+    projectimg:"https://maheshkumar77.github.io/Portfolio_Use_Nextjs/project1.jpg",
+    technology: ["Reactjs", "Tailwindcss","Javascript", "Nodejs", "ExpressJs", "Mongodb"],
+    
+  },
+  {
+    id: 2,
+    projectname: "eCommerce Website",
+    
+    projectdesc:`This eCommerce website is built using React, Material UI, 
+    and Bootstrap for a responsive and user-friendly interface, with Razorpay integration for secure payments.
+     The backend is powered by Node.js, and data is efficiently stored in 
+    MongoDB for seamless management.`,
+    projectlink1: "https://github.com/maheshkumar77/E-Commerce-Website",
+    projectlink2l: "https://ecoshophear.netlify.app/",
+    projectimg:"https://maheshkumar77.github.io/Portfolio_Use_Nextjs/ecomerce.jpg",
+    technology: ["Reactjs","Javascript",  "Tailwindcss", "Nodejs", "ExpressJs", "Mongodb","RestApi","Razorpay Payment Gateway"],
+  },
+  {
+    id: 3,
+    projectname: "Recipi Application",
+    
+    projectdesc:`The Recipe Application utilizes REST API calls to fetch data, with a responsive frontend built using React, TypeScript, and Tailwind CSS. It features a search bar to 
+    find food recipes and provides detailed recipe instructions along with YouTube video links.`,
+    projectlink1: "https://github.com/maheshkumar77/Food-Recipe-Application",
+    projectlink2l: "https://github.com/maheshkumar77/Food-Recipe-Application",
+    projectimg:"https://maheshkumar77.github.io/Portfolio_Use_Nextjs/recipi.jpg",
+    technology: ["Reactjs", "Tailwindcss","RestApi","Typescript"],
+  },
+  {
+    id: 4,
+    projectname: "My portfulio",
+    
+    projectdesc:`A portfolio website highlights an individual's skills, projects, and achievements,
+     offering a professional online presence. It helps enhance personal branding, attract clients or employers, and showcase past work effectively.`,
+    projectlink1: "https://github.com/maheshkumar77/New_Portfulio",
+    projectlink2l: "https://maheshkumar77.github.io/NewPortfolio/",
+    projectimg:"https://maheshkumar77.github.io/Portfolio_Use_Nextjs/chat.jpg",
+    technology: ["Nextjs","Typescript","AceternityUi","Tailwindcss"],
+  },
+  
+];
